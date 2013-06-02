4d2735537ec51f0f7d63f93041b26f2098ca8944
eric mcgregor <eric.mcgregor@rackspace.com>
2013-06-02 13:06:42 -0500

    homepage changes

--- agenda.php
+++ agenda.php
@@ -1,49 +1,3 @@
-<?php
-
-// 	//require_once 'libs/Mongo_db.php';
-// 	require_once 'libs/Twilio.php';
-	
-// 	$sid = "AC5a93d04a51ef297e78f2518892f1a939"; // Your Account SID from www.twilio.com/user/account
-// 	$token = "6802051d3cb9f0ffdbba46c138087c8f"; // Your Auth Token from www.twilio.com/user/account
-
-// 	$client = new Services_Twilio($sid, $token);
-
-// $m = new Mongo('166.78.181.9');
-// $db = $m->test;
-// $collection = $db->test;
-
-
-// print_r($collection);
-
-// //$mdb = new Mongo_db('166.78.181.194');
-
-// //$connection = new MongoClient('166.78.181.194');
-// //$connection = new MongoClient();
-// //	phpinfo();
-
-// 	$message = $client->account->sms_messages->create(
-// 		'9172439244', // From a valid Twilio number
-// 		'6465097500', // Text this number
-// 		"Hello monkey!"
-// 	);
-
-
-// //phpinfo();
-
-// $item = array(
-// 	'agenda_id'=>'07161980',
-// 	'item_id'=>'1111',
-// 	'type'=>'Austin Energy',
-// 	'content'=>'testtestest'
-// 	);
-// $item = json_encode($item);
-
-// //echo $item;
-
-
-
-
-?>
 <!DOCTYPE html>
     <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
     <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
@@ -71,81 +25,10 @@
         <![endif]-->
  
 
-	<div class="hero-unit">
-		<div class="container">
-			<h1>Agenda Alerts</h1>
-			<p class="lead">Find out what Austin City Council is up to in your zip code.</p>
-
-			<div class="well">
-				<div id="hero_form" class="row-fluid">
-					<div class="span6 form_content">
-						<form class="form-inline">
-							<fieldset>
-								<h2>Get Alerts!</h2>
-								<p>We will text you when City Council is talking about your neighborhood.</p>
-								<div class="control-group">
-									<label class="control-label" for="input01">Phone</label>
-									<div class="controls">
-										<input type="text" class="input-xlarge" id="input01">
-									</div>
-
-								</div>	
-								<div class="control-group">
-									<label class="control-label" for="input01">Zip</label>
-									<div class="controls">
-										<input type="text" class="input-xlarge" id="input01">
-									</div>
-								</div>									
-								<div class="control-group">
-									<label class="control-label" for="input01">Email</label>
-									<div class="controls">
-										<input type="text" class="input-xlarge" id="input01">
-									</div>
-								</div>
-								<div class="form-actions">
-									<button type="submit" class="btn btn-primary btn-large btn-block">Get Alerts</button>
-								</div>
-							</fieldset>
-						</form>	
-						<br>
-						<form class="form form-unsubscribe">
-							<fieldset>
-								<h4>Unsubscribe from alerts right here</h4>
-								<div class="control-group">
-
-									<div class="controls">
-										<label class="control-label" for="input01">Phone</label>							
-										<input type="text" class="input-xlarge" id="input01">
-									</div>
-
-								</div>
-
-								<div class="form-actions">
-									<button type="submit" class="btn btn-warning btn-block">Unsubscribe from Alerts</button>
-								<p class="help-block"><strong>Or...</strong> text "stop" to 917-243-9244</p>
-
-								</div>
-
-
-							</fieldset>
-						</form>													
-					</div>
-					<div class="span6 form_image">
-						<img src="application/img/text-screen.png" alt="">
-					</div>
-				</div>
-
-			
-
-			</div>
-
-		</div>
-	</div>
-
-	<br>
-
+<div class="hero-unit">
 	<div class="container">
 		<h1>Upcoming Austin City Council Agenda</h1>
+		<br>
 		<table class="table">
 			<tbody>
 				<tr>
@@ -162,6 +45,12 @@
 				</tr>
 			</tbody>
 		</table>
+	</div>
+</div>
+
+
+	<div class="container">
+
 
 		<dl class="agenda-list custom-agenda">
 			<dt class="category austin-energy">Agenda items for your zip code - 78702</dt>
@@ -257,18 +146,6 @@
 	</div>
 
 
-
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-
-<?php include('footer.php'); ?>
 
  
         <!-- JavaScript libraries -->
---
+++ textapi.php
@@ -0,0 +1,116 @@
+<?php
+	require_once 'libs/Twilio.php';
+
+	$sid = "AC5a93d04a51ef297e78f2518892f1a939"; // Your Account SID from www.twilio.com/user/account
+	$token = "6802051d3cb9f0ffdbba46c138087c8f"; // Your Auth Token from www.twilio.com/user/account
+
+	$client = new Services_Twilio($sid, $token);
+
+
+	//GET THE TEXT MESSAGE
+	$body = $_REQUEST['Body'];
+	$from = $_REQUEST['From'];
+
+
+	//FIGURE OUT IF TEXT IS A ZIP CODE
+	if(is_numeric($body) && strlen($body) == 5) {
+
+		//if user exists then update their zip code of interest
+		if(user_exists($from)) {
+			//UPDATE USER ZIPCODE
+			update_user_helper($from, $body);
+
+		} else {
+			//ADD A USER
+			add_user_helper($from, $body);
+		}
+
+	} else {
+
+		if($body == 'stop' || $body == 'Stop') {
+			delete_user_helper($from);
+		}
+	}
+
+
+
+
+	//UNSUBSCRIBE
+
+	function delete_user_helper($from) {
+
+		//UNSUBSCRIBE USER
+		unsubscribe_user($_REQUEST['From']);
+
+
+		//send message
+		$m = "You've been unsubscribed";
+		send_message($m, $from);
+
+	}
+
+
+	
+	//ADD USER FOR ZIP CODE
+	function add_user_helper($from, $body) {
+		//ADD USER
+		subscribe_user($_REQUEST['From'], $_REQUEST['Body']);
+
+		//send message
+		$m = "Thanks for subscribing to Agenda Alerts.  You'll receieve alerts for zip code {$_REQUEST['Body']}.  Text 'stop' top unsubscribe.";
+		send_message($m, $from);
+	}
+
+
+	//UPDATE USER ZIP CODE
+	function update_user_helper($from, $zip) {
+		//UPDATE USER
+		update_user($from, $zip);
+
+		//send message
+		$m = "Agenda Alerts has updated your zip code to {$zip}.";
+		send_message($m, $from);
+	}
+
+
+	//SEND A MESSAGE
+	function send_message($m, $phone) {
+
+		$message = $client->account->sms_messages->create(
+				'9172439244', // From a valid Twilio number
+				$phone, // Text this number
+				$m
+			);
+
+	}
+
+	function unsubscribe_user($phone) {
+		return true;
+	}
+
+	function subscribe_user($phone, $zip) {
+		return true;
+	}
+
+	function user_exists($phone) {
+		return true;
+	}
+
+	function update_user($zip) {
+		return true;
+	}
+
+
+
+
+
+
+	// $message = $client->account->sms_messages->create(
+	// 	'9172439244', // From a valid Twilio number
+	// 	'6465097500', // Text this number
+	// 	"Hello monkey!"
+	// );
+
+
+
+?>
